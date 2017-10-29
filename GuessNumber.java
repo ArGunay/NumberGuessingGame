c6bc58f42aaa875a3558b99b463667ba2b5abb46
Ardil Gunay <guenaa@usi.ch>
2017-10-29 12:40:26 +0100

    added reply method... still not working

--- GuessNumber.java
+++ GuessNumber.java
@@ -24,32 +24,42 @@ public class GuessNumber {
         //         System.out.println("EXEP");
         //     }
         // }   
-
+        
         
         try{
-
             Socket clientSocket = new Socket("research.inf.usi.ch",9999);
             game.connectionMethod(clientSocket);
+            System.out.println("END-CONNECTION-METHOD");
+            game.replyMethod(clientSocket, game.cookie);
+            System.out.println("END-REPLY-METHOD");
+            // clientSocket.close();
         } catch (IOException e){ //can never happen}
-
             System.out.println("EXEP");
         }
+        System.out.println("END-OF-TRY");
     }
 
+    String cookie;
+
+    // public String getCookie(){
+    //     return this.cookie;
+
+    // }
     
 
 
     public void connectionMethod(Socket clientSocket)
     throws IOException {
-        String host = clientSocket.getInetAddress().getHostName();
-        Integer port = clientSocket.getPort();
+
+        String host = clientSocket.getInetAddress().getHostName(); 
+        System.out.println("HOST IS: "+ host);
+        // Integer port = clientSocket.getPort();
         String path = "new-player";
 
         // Opening Connection based on the port number 80(HTTP) and 443(HTTPS)
 
         
-
-        System.out.println("PORT: "+clientSocket.getPort()+ " inet: "+ clientSocket.getInetAddress().getHostName());
+        System.out.println("PORT: "+clientSocket.getPort()+ " inet: "+ clientSocket.getInetAddress());
 
         System.out.println();
 
@@ -85,25 +95,85 @@ public class GuessNumber {
             System.out.println(responseLine);
             li.add(responseLine);
 
+
         }
 
         // get 3rd element of the arraylist that is the set-cookie strig
         String l = li.get(3);
 
         // get the cookie number
-        String cookie = l.substring(l.indexOf("=")+1,l.length());
-        System.out.println("cookie = "+ cookie);
+        this.cookie = l.substring(l.indexOf("=")+1,l.length());
+        System.out.println("cookie = "+ this.cookie);
 
         System.out.println("======================================");
         System.out.println("Response Recieved!!");
         System.out.println("======================================");
 
-        response.close();
-        request.close();
-        clientSocket.close();
+        // response.close();
+        // request.close();
+        
     }
 
 
+    // ==================================================================================
+    // ==================================           =====================================
+    // ==================================   REPLY   =====================================
+    // ==================================           =====================================
+    // ==================================================================================
+
+
+    public void replyMethod(Socket clientSocket, String cookie){
+        
+        String host = clientSocket.getInetAddress().getHostName(); 
+        String path = "check-number";
+
+        try{
+
+        
+            PrintWriter request = new PrintWriter(clientSocket.getOutputStream(),true);
+            System.out.println("POST REQ " + request);
+            // Declare a listener to this url
+            BufferedReader response = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("POST RESP " + response);
+
+            // Sending request to the server
+            // Building HTTP request header
+            request.print("POST /" + path + "/ HTTP/1.1\r\n");      System.out.println("POST PATH " + path);
+            request.print("Host: " + host + "\r\n");                System.out.println("POST HOST " + host);
+            request.print("Connection: open\r\n");
+            request.print("cookie: "+ cookie + "\r\n");             System.out.println("cookie: "+ cookie);
+            request.print("number: " + 2 + "\r\n");
+            request.print("Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\r\n");
+            request.print("\r\n");
+            request.flush();
+            System.out.println("POST REQ Sent!");
+            System.out.println("======================================");
+
+            // Receiving response from server
+            String responseLine;
+            System.out.println("RESPONSEEEE: "+ response.readLine());
+            //        String beacon = l.substring(l.indexOf("=")+1,l.length());
+            List<String> li = new ArrayList<>();
+
+            while ((responseLine = response.readLine()) != null) 
+            {
+                
+                System.out.println(responseLine);
+                li.add(responseLine);
+
+            }
+
+            System.out.println("======================================");
+            System.out.println("POST Response Recieved!!");
+            System.out.println("======================================");
+            
+        } 
+        catch(IOException e)
+        { 
+            System.out.println("REPLY METHOD ERROR " + e);
+        }
+    }
+}
 
 
     // =================================================================================
@@ -194,4 +264,3 @@ public class GuessNumber {
 
 
 
-}
